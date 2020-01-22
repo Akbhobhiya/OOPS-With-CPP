b3b9af103707752ba620c72d44c6b23afdcdc45c
Ashok Bhobhiya <akbhobhiya2000@gmail.com>
2020-01-23 00:20:34 +0530

    Add files via upload

---
+++ lab1/prob6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"enter the real part and imagine part of complex number"<<endl;
+	cin>>a>>b;
+}
+class complex
+{
+private:
+	int x,y;
+	
+}
