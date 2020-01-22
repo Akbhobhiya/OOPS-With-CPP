529fc853d4c3af996c0986ab68f1cc227a0c6d04
Ashok Bhobhiya <akbhobhiya2000@gmail.com>
2020-01-23 00:28:05 +0530

    Update prob6.cpp

--- lab2/prob6.cpp
+++ lab2/prob6.cpp
@@ -1,46 +1,66 @@
-#include<iostream>
-using namespace std;
+#include<iostream> 
+using namespace std; 
+class Complex { 
+private: 
+	int real, imag; 
+public: 
+	Complex(int r = 0, int i =0) 
+	{
+		real = r; imag = i;
+    } 
+	Complex operator ++ ()
+	{ 
+		Complex res; 
+		res.real = ++real;
+		res.imag = ++imag; 
+		return res; 
+	} 
 
-class complex{
-public:
-	int real,img;
-	complex(int a,int b);
-	void operator++();
-	void operator--();
-};
- complex::complex(int a, int b){
- 	real=a;
- 	img=b;
- }
+	Complex operator --()
+	{
+		Complex p;
+		p.real=--real;
+		p.imag=--imag;
+		return p;
+	}
+	Complex operator ++(int)
+	{
+		Complex res; 
+		res.real = ++real;
+		res.imag = ++imag; 
+		return res; 
+	}
+	Complex operator --(int)
+	{
+		Complex p;
+		p.real=--real;
+		p.imag=--imag;
+		return p;
+	}
+	void print()
+	{
+	    cout << real << " + i" << imag << endl;
+	} 
+}; 
 
- void complex::operator++(){
- 	real++;
- 	img++;
- 	cout<<real<<" "<<img;
- }
-
- void complex::operator--(){
- 	real--;
- 	img--;
- 	cout<<"\n"<<real<<" "<<img;
- }
-
-
-
-
-int main(){
-	int c, b;
-	cin>>c>>b;
-  complex a(c,b);
-  ++a;
-  --a;
-
-}
-
-
-
-
-
-
+int main() 
+{ 
+	int x,y;
+	cout<<"enter the real part and imagine part of Complex number"<<endl;
+	cin>>x>>y;
+	Complex c1(x,y); 
+	cout<<"pre incriment operator"<<endl;
+	Complex c3 = ++c1; // An example call to "operator++" 
+	c3.print();
+	cout<<"pre decrement operator"<<endl;
+	Complex c4=--(c1);
+	c4.print();
+	cout<<"post incriment operator"<<endl;
+	Complex c5=c1++;
+	c5.print();
+	cout<<"post decrement operator"<<endl;
+	Complex c6=c1--;
+	c6.print();
+} 
 
 
